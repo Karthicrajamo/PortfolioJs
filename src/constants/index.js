46c19504153f880be8bfb134ab714e51f0a7002a
Karthic Raja <karthicraja@neulancers.com>
2024-05-29 23:11:55 +0530

    Adding Experiance

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,84 +7,131 @@ import logo from "../assets/images/logo.png";
 
 export { hero, meta, shopify, starbucks, tesla, logo };
 
+import css from "../assets/icons/css.svg";
+import express from "../assets/icons/express.svg";
+import git from "../assets/icons/git.svg";
+import github from "../assets/icons/github.svg";
+import html from "../assets/icons/html.svg";
+import javascript from "../assets/icons/javascript.svg";
+import mongodb from "../assets/icons/mongodb.svg";
+import motion from "../assets/icons/motion.svg";
+import mui from "../assets/icons/mui.svg";
+import nextjs from "../assets/icons/nextjs.svg";
+import nodejs from "../assets/icons/nodejs.svg";
+import react from "../assets/icons/react.svg";
+import redux from "../assets/icons/redux.svg";
+import sass from "../assets/icons/sass.svg";
+import tailwindcss from "../assets/icons/tailwindcss.svg";
+import typescript from "../assets/icons/typescript.svg";
+import linkedin from "../assets/icons/linkedin.svg";
+import twitter from "../assets/icons/twitter.svg";
+import car from "../assets/icons/car.svg";
+import estate from "../assets/icons/estate.svg";
+import pricewise from "../assets/icons/pricewise.svg";
+import snapgram from "../assets/icons/snapgram.svg";
+import summiz from "../assets/icons/summiz.svg";
+import threads from "../assets/icons/threads.svg";
+import arrow from "../assets/icons/arrow.svg";
+import contact from "../assets/icons/contact.svg";
+import soundon from "../assets/icons/soundon.png";
+import soundoff from "../assets/icons/soundoff.png";
+import java from "../assets/icons/icons8-java.svg";
+import python from "../assets/icons/icons8-python.svg";
+import cpp from "../assets/icons/icons8-c++.svg";
+import c from "../assets/icons/icons8-c.svg";
+import php from "../assets/icons/icons8-php.svg";
+import dj from "../assets/icons/django.png";
+import neulancers from "../assets/icons/neulancers-logo-white.png";
 
-import css from '../assets/icons/css.svg'
-import express from '../assets/icons/express.svg'
-import git from '../assets/icons/git.svg'
-import github from '../assets/icons/github.svg'
-import html from '../assets/icons/html.svg'
-import javascript from '../assets/icons/javascript.svg'
-import mongodb from '../assets/icons/mongodb.svg'
-import motion from '../assets/icons/motion.svg'
-import mui from '../assets/icons/mui.svg'
-import nextjs from '../assets/icons/nextjs.svg'
-import nodejs from '../assets/icons/nodejs.svg'
-import react from '../assets/icons/react.svg'
-import redux from '../assets/icons/redux.svg'
-import sass from '../assets/icons/sass.svg'
-import tailwindcss from '../assets/icons/tailwindcss.svg'
-import typescript from '../assets/icons/typescript.svg'
-import linkedin from '../assets/icons/linkedin.svg'
-import twitter from '../assets/icons/twitter.svg'
-import car from '../assets/icons/car.svg'
-import estate from '../assets/icons/estate.svg'
-import pricewise from '../assets/icons/pricewise.svg'
-import snapgram from '../assets/icons/snapgram.svg'
-import summiz from '../assets/icons/summiz.svg'
-import threads from '../assets/icons/threads.svg'
-import arrow from '../assets/icons/arrow.svg'
-import contact from '../assets/icons/contact.svg'
-import soundon from '../assets/icons/soundon.png'
-import soundoff from '../assets/icons/soundoff.png'
+export const skills = [
+	{ imageUrl: css, name: "CSS", type: "Frontend" },
+	{
+		imageUrl: git,
+		name: "Git",
+		type: "Version Control",
+	},
+	{
+		imageUrl: github,
+		name: "Github",
+		type: "Version Control",
+	},
+	{
+		imageUrl: html,
+		name: "HTML",
+		type: "Frontend",
+	},
+	{
+		imageUrl: javascript,
+		name: "Javascript",
+		type: "Frontend",
+	},
+	{
+		imageUrl: mongodb,
+		name: "Mongodb",
+		type: "Backend",
+	},
+	{
+		imageUrl: java,
+		name: "Java",
+		type: "Backend",
+	},
+	{
+		imageUrl: python,
+		name: "Python",
+		type: "Backend",
+	},
+	{
+		imageUrl: c,
+		name: "C",
+		type: "Backend",
+	},
+	{
+		imageUrl: cpp,
+		name: "C++",
+		type: "Backend",
+	},
+	{
+		imageUrl: php,
+		name: "php",
+		type: "Backend",
+	},
+	{
+		imageUrl: dj,
+		name: "Django",
+		type: "Backend",
+	},
+	{
+		imageUrl: nextjs,
+		name: "NextJs",
+		type: "Frontend",
+	},
+	{
+		imageUrl: react,
+		name: "ReactJs",
+		type: "Frontend",
+	},
+	{
+		imageUrl: tailwindcss,
+		name: "TailwindCSS",
+		type: "Frontend",
+	},
+	{
+		imageUrl: typescript,
+		name: "Typescript",
+		type: "Frontend",
+	},
+];
 
-export const skills = [{imageUrl: css,
-    name:"CSS",
-    type: "Frontend"
-},
-{
-    imageUrl: git,
-    name: "Git",
-    type: "Version Control"
-},
-{
-   imageUrl: github,
-   name: "Github",
-   type: "Version Control"
-},
-{
-    imageUrl: html,
-    name: "HTML",
-    type: "Frontend"
- },
- {
-    imageUrl: javascript,
-    name: "Javascript",
-    type: "Frontend"
- },
- {
-    imageUrl: mongodb,
-    name: "Mongodb",
-    type: "Backend"
- },
- {
-    imageUrl: nextjs,
-    name: "NextJs",
-    type: "Frontend"
- },
- {
-    imageUrl: react,
-    name: "ReactJs",
-    type: "Frontend"
- },
- {
-    imageUrl: tailwindcss,
-    name: "TailwindCSS",
-    type: "Frontend"
- },
- {
-    imageUrl: typescript,
-    name: "Typescript",
-    type: "Frontend"
- },
-    
-]
+export const experience = [
+	{
+		title: "FullStack Developer",
+		company_name: "Neulancers",
+		icon: neulancers,
+		iconBg: "#accbe1",
+		date: "2023 - 2024",
+		points: [
+			"Led the end-to-end development of webapp, serving as both Backend and Frontend developer, with a focus on implementing dynamic rendering for the Product page using NextJs.",
+			"Worked with a team to plan and build  Ecommerce website as a backend developer using Django.",
+		],
+	},
+];
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,16 @@
 // import React from 'react'
 
-import { div } from "three/examples/jsm/nodes/Nodes.js";
-import { skills } from "../constants";
+import { experience, skills } from "../constants";
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 
 export const About = () => {
 	return (
 		<section className="max-container">
 			<h1 className="head-text">
-				Hello, I'm{" "}
+				Hello, I&apos;m{" "}
 				<span className="blue-gradient_text font-semibold drop-shadow-sm">
 					{" "}
 					Karthic Raja
@@ -26,12 +29,66 @@ export const About = () => {
 					{skills.map((skill, index) => {
 						return (
 							<div className="block-container w-20 h-20" key={index}>
-								<div className="btn-back rounded-xl"/><div className="btn-front rounded-xl flex justify-center items-center">
-                  <img src={skill.imageUrl} alt={skill.name} className="w-1/2 h-1/2 object-contain"/>
-                </div>
+								<div className="btn-back rounded-xl" />
+								<div className="btn-front rounded-xl flex justify-center items-center">
+									<img
+										src={skill.imageUrl}
+										alt={skill.name}
+										className="w-1/2 h-1/2 object-contain"
+									/>
+								</div>
 							</div>
 						);
 					})}
+				</div>
+			</div>
+			<div className="py-16">
+				<h3 className="subhead-text">Work Experience</h3>
+				<div className="mt-5 flex flex-col gap-3 text-slate-500">
+					<p>
+						I&apos;mve worked at Neulancers, continuously leveling up my skills
+						and collaborating with talented individuals. Here&apos;ms the
+						rundown:
+					</p>
+				</div>
+				<div className="mt-12 flex">
+					<VerticalTimeline>
+						{experience.map((experiences, index) => {
+							return (
+								<VerticalTimelineElement
+									key={index}
+									date={experiences.date}
+									iconStyle={{ background: experiences.iconBg }}
+									icon={
+										<div className="flex justify-center items-center h-full w-full ">
+											<img
+												src={experiences.icon}
+												alt={experiences.company_name}
+												className="w-[10%] h-[10%] object-contain"
+											/>
+										</div>
+									}
+									// contentStyle={{
+									// 	borderBottom: "8px",
+									// 	borderStyle:"solid",
+									// 	borderBottomColor: experiences.iconBg,
+									// 	boxShadow: "none",
+									// }}
+								>
+									<div>
+										<h3>{experiences.title}</h3>
+										<p>{experiences.company_name}</p>
+									</div>
+
+									<ul>
+										{experiences.points.map((point, index) => {
+											return <li key={index} className="w-[45%]">{point}</li>;
+										})}
+									</ul>
+								</VerticalTimelineElement>
+							);
+						})}
+					</VerticalTimeline>
 				</div>
 			</div>
 		</section>
